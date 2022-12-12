9a3f5f80ba3dcad4ce42411da008074fe576c204
dostalm.04 <dostalm.04@spst.eu>
2022-12-12 11:59:36 +0100

    Přidejte soubory projektu.

---
+++ Funkce01/Funkce01.cpp
@@ -0,0 +1,19 @@
+﻿// Funkce01.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	 
+
+
+
+
+
+
+
+
+}
+
